890984d20d5b472f201699b3d8cf223fccb704a1
宮崎 洋史 <h@myzk.me>
2015-12-07 09:21:24 +0900

    secret_keyとconfig.pepperを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,4 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :email, presence: true
-
 end
